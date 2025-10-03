266581a156c538d1d4340f2f83d420a27d7907c4
Kevin <101434732+kevinhuytran1128@users.noreply.github.com>
2025-10-02 22:16:38 -0400

    update

--- date2day/src/App.tsx
+++ date2day/src/App.tsx
@@ -1,41 +1,49 @@
 import "./App.css";
-
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import Banner from "./components/custom/Banner";
+import { createRoot } from "react-dom/client";
 import {
     APIProvider,
-    AdvancedMarker,
     Map,
-    Marker,
+    AdvancedMarker,
     Pin,
+    useMap,
 } from "@vis.gl/react-google-maps";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import type { Marker } from "@googlemaps/markerclusterer";
 
-import Banner from "./components/custom/Banner";
-import type { MapCameraChangedEvent } from "@vis.gl/react-google-maps";
-import { useState } from "react";
-
+// Toronto hotspots
 type Poi = { key: string; location: google.maps.LatLngLiteral };
 const locations: Poi[] = [
-    { key: "operaHouse", location: { lat: -33.8567844, lng: 151.213108 } },
-    { key: "tarongaZoo", location: { lat: -33.8472767, lng: 151.2188164 } },
-    { key: "manlyBeach", location: { lat: -33.8209738, lng: 151.2563253 } },
-    { key: "hyderPark", location: { lat: -33.8690081, lng: 151.2052393 } },
-    { key: "theRocks", location: { lat: -33.8587568, lng: 151.2058246 } },
-    { key: "circularQuay", location: { lat: -33.858761, lng: 151.2055688 } },
-    { key: "harbourBridge", location: { lat: -33.852228, lng: 151.2038374 } },
-    { key: "kingsCross", location: { lat: -33.8737375, lng: 151.222569 } },
-    { key: "botanicGardens", location: { lat: -33.864167, lng: 151.216387 } },
-    { key: "museumOfSydney", location: { lat: -33.8636005, lng: 151.2092542 } },
-    { key: "maritimeMuseum", location: { lat: -33.869395, lng: 151.198648 } },
-    {
-        key: "kingStreetWharf",
-        location: { lat: -33.8665445, lng: 151.1989808 },
-    },
-    { key: "aquarium", location: { lat: -33.869627, lng: 151.202146 } },
-    { key: "darlingHarbour", location: { lat: -33.87488, lng: 151.1987113 } },
-    { key: "barangaroo", location: { lat: -33.8605523, lng: 151.1972205 } },
+    { key: "cnTower", location: { lat: 43.6426, lng: -79.3871 } },
+    { key: "rogersCentre", location: { lat: 43.6414, lng: -79.3894 } },
+    { key: "unionStation", location: { lat: 43.6455, lng: -79.3807 } },
+    { key: "scotiabankArena", location: { lat: 43.6435, lng: -79.3791 } },
+    { key: "royalOntarioMuseum", location: { lat: 43.6677, lng: -79.3948 } },
+    { key: "artGalleryOntario", location: { lat: 43.6536, lng: -79.3925 } },
+    { key: "stLawrenceMarket", location: { lat: 43.6487, lng: -79.3716 } },
+    { key: "distilleryDistrict", location: { lat: 43.6506, lng: -79.3596 } },
+    { key: "highPark", location: { lat: 43.6465, lng: -79.4637 } },
+    { key: "torontoIslands", location: { lat: 43.6205, lng: -79.3783 } },
+    { key: "cityHall", location: { lat: 43.6535, lng: -79.3841 } },
+    { key: "eatonCentre", location: { lat: 43.6544, lng: -79.3807 } },
+    { key: "yorkville", location: { lat: 43.671, lng: -79.3934 } },
+    { key: "kensingtonMarket", location: { lat: 43.654, lng: -79.4001 } },
+
+    { key: "ontarioScienceCentre", location: { lat: 43.7161, lng: -79.3389 } },
+    { key: "cn1", location: { lat: 43.6426, lng: -79.3871 } },
+    { key: "cn2", location: { lat: 43.6427, lng: -79.3872 } },
+    { key: "cn3", location: { lat: 43.6428, lng: -79.3873 } },
+    { key: "cn4", location: { lat: 43.6425, lng: -79.387 } },
+    { key: "cn5", location: { lat: 43.6424, lng: -79.3872 } },
+    { key: "cn6", location: { lat: 43.6429, lng: -79.3871 } },
+    { key: "cn7", location: { lat: 43.643, lng: -79.3874 } },
+    { key: "cn8", location: { lat: 43.6423, lng: -79.3873 } },
+    { key: "cn9", location: { lat: 43.6422, lng: -79.3875 } },
+    { key: "cn10", location: { lat: 43.6421, lng: -79.3876 } },
 ];
 
 function App() {
-    const [mapLoaded, setMapLoaded] = useState<boolean>(false);
     return (
         <>
             <Banner />
@@ -49,8 +57,9 @@ function App() {
                     <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_KEY}>
                         <Map
                             defaultZoom={13}
-                            defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+                            defaultCenter={{ lat: 43.6532, lng: -79.3832 }}
                             style={{ width: "100%", height: "400px" }}
+                            mapId={"DEMO"}
                         >
                             <PoiMarkers pois={locations} />
                         </Map>
@@ -61,11 +70,58 @@ function App() {
     );
 }
 
-const PoiMarkers = (props: { pois: Poi[] }) => {
+const PoiMarkers = ({ pois }: { pois: Poi[] }) => {
+    const map = useMap();
+    const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+    const clusterer = useRef<MarkerClusterer | null>(null);
+
+    useEffect(() => {
+        if (!map) return;
+        if (!clusterer.current) {
+            clusterer.current = new MarkerClusterer({ map });
+        }
+    }, [map]);
+
+    useEffect(() => {
+        clusterer.current?.clearMarkers();
+        clusterer.current?.addMarkers(Object.values(markers));
+    }, [markers]);
+
+    const setMarkerRef = (marker: Marker | null, key: string) => {
+        if (marker && markers[key]) return;
+        if (!marker && !markers[key]) return;
+
+        setMarkers((prev) => {
+            if (marker) {
+                return { ...prev, [key]: marker };
+            } else {
+                const newMarkers = { ...prev };
+                delete newMarkers[key];
+                return newMarkers;
+            }
+        });
+    };
+
+    const handleClick = useCallback(
+        (ev: google.maps.MapMouseEvent) => {
+            if (!map) return;
+            if (!ev.latLng) return;
+            console.log("marker clicked:", ev.latLng.toString());
+            map.panTo(ev.latLng);
+        },
+        [map] // 👈 dependency array
+    );
+
     return (
         <>
-            {props.pois.map((poi: Poi) => (
-                <AdvancedMarker key={poi.key} position={poi.location}>
+            {pois.map((poi: Poi) => (
+                <AdvancedMarker
+                    key={poi.key}
+                    position={poi.location}
+                    ref={(marker) => setMarkerRef(marker, poi.key)}
+                    clickable={true}
+                    onClick={handleClick}
+                >
                     <Pin
                         background={"#FBBC04"}
                         glyphColor={"#000"}
